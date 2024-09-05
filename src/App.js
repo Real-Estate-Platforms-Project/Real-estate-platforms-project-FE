1f55cc6e759bc4ddb137bc8d0803e5e00df145a4
truonghoi1205 <phamtruonghoi.dmx@gmail.com>
2024-09-05 11:16:04 +0700

    fix 404 page

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import './App.css';
 import './css/custom.css'
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './page/client/Home';
+import Client from './page/layout/Client';
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NotFound from "./component/client/404";
 
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Home />} >
-                    <Route path="*" element={<NotFound />} />
-                </Route>
-            </Routes>
-            <ToastContainer />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Client />}>
+            <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
