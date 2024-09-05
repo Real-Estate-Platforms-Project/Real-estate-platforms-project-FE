0b4d3e5b1bab4b0dfd98e28964a1e1a8a819423f
duongnhat0801 <nhatx0801@hmail.com>
2024-09-05 10:56:02 +0700

    fuckon-

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './page/client/Home';
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFound from "./component/client/404";
 
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-        </Route>
-      </Routes>
-      <ToastContainer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Home />} >
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+            <ToastContainer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
