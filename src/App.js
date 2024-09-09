fc045e38d5f1472d217047aa4462fde43f26ebef
quangle0309 <quang9999xx.1@gmail.com>
2024-09-09 09:48:24 +0700

    update luu tru token va dang nhap dang ky

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import './css/custom.css'
 import "./css/SearchBar.css"
-import { ToastContainer } from "react-toastify";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Client from './page/layout/Client';
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,25 +12,30 @@ import CreateRealEstate from "./page/client/CreateRealEstate";
 import Carousel from "./component/carousel/Carousel";
 import Notification from "./component/client/Notification";
 import NotificationDetail from "./component/client/NotificationDetail";
-
+import Login from "./page/auth/Login";
+import Register from "./page/auth/Register";
+import ActivationSuccess from "./page/auth/ConfirmEmail";
 
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Client />}>
-            {/*<Route path="/" element={<Home/>} />*/}
-            <Route path="/sellernet/dang-tin" element={<CreateRealEstate/>} />
-            <Route path="*" element={<NotFound/>} />
-            <Route path="/" element={<Carousel/>} />
-            <Route path="/notification" element={<Notification/>} />
-            <Route path="/notificationDetail/:id" element={<NotificationDetail />} />
-        </Route>
-      </Routes>
-      <ToastContainer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/activation-success" element={<ActivationSuccess />} />
+                <Route path="/" element={<Client/>}>
+                    {/*<Route path="/" element={<Home/>} />*/}
+                    <Route path="/sellernet/dang-tin" element={<CreateRealEstate/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                    <Route path="/" element={<Carousel/>}/>
+                    <Route path="/notification" element={<Notification/>}/>
+                    <Route path="/notificationDetail/:id" element={<NotificationDetail/>}/>
+                </Route>
+            </Routes>
+            <ToastContainer/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
