5bb0bf51da5057f3489f961eb7eae3504aef962a
HieuTranROM <trunghieu17082001@gmail.com>
2024-09-11 14:20:07 +0700

    pull main

--- src/component/customer/CustomerAddForm.js
+++ src/component/customer/CustomerAddForm.js
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 
 const CustomerAddForm = () => {
+    const [name, setName] = useState('');
     const [customer, setCustomer] = useState({
         name: '',
         email: '',
