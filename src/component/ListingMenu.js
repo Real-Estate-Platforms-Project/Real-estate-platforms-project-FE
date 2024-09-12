b8eb4eef99ae8778a5eaf734343534776d771670
Phan Thanh <phanchithanh91@gmail.com>
2024-09-12 10:13:09 +0700

    thanh update

--- src/component/ListingMenu.js
+++ src/component/ListingMenu.js
@@ -4,8 +4,8 @@ export default function ListingMenu() {
     return (
         <div className="d-flex text-center">
             <div>
-                <Link className="dropdown-item text-dark fw-bold" to='buyernet/danh-sach-nhu-cau'>Danh sach nhu cau</Link>
-                <Link className="dropdown-item text-dark fw-bold" to='/estate-list'>Danh sach nhà Đất</Link>
+                <Link className="dropdown-item text-dark fw-bold" to='buyernet/danh-sach-nhu-cau'>Danh sách nhu cầu</Link>
+                <Link className="dropdown-item text-dark fw-bold" to='/estate-list'>Danh sách nhà Đất</Link>
             </div>
         </div>
     )
