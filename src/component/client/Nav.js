9a6b95fa5c819a41515a847979984a7ef4649e96
Phan Thanh <phanchithanh91@gmail.com>
2024-09-16 15:01:34 +0700

    thanh update phan trang tim kiem nhu cau

--- src/component/client/Nav.js
+++ src/component/client/Nav.js
@@ -14,7 +14,6 @@ function Nav() {
     const isSeller = roles.some(role => role.name === 'ROLE_SELLER' || role.name === 'ROLE_ADMIN' || role.name === 'ROLE_EMPLOYEE')
     console.log(isAuthenticated)
 
-
     return (
         <div>
             <nav className="navbar navbar-expand-lg">
