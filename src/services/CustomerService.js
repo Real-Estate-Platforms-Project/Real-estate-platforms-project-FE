99b8252996207244f456740de179e7f069c1415d
HieuTranROM <trunghieu17082001@gmail.com>
2024-09-16 16:00:14 +0700

    update up anh customer

--- src/services/CustomerService.js
+++ src/services/CustomerService.js
@@ -1,22 +1,36 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-export const addCustomer = async (values) => {
+export const addCustomer = async (formData) => {
     try {
-        await axios.post('http://localhost:8080/api/customers/add', values);
+        await axios.post('http://localhost:8080/api/customers/add', formData, {
+        });
         toast.success('Khách hàng đã được thêm thành công.', {
             position: "top-right",
             autoClose: 3000,
         });
     } catch (error) {
-        console.error('Error adding customer:', error);
+        console.error('Error adding customer:', error.response ? error.response.data : error.message);
+        if (error.response) {
+            toast.error(`Lỗi khi thêm khách hàng: ${error.response.data.message}`, {
+                position: "top-right",
+                autoClose: 3000,
+            });
+        } else {
+            toast.error('Lỗi kết nối. Vui lòng thử lại.', {
+                position: "top-right",
+                autoClose: 3000,
+            });
+        }
         throw error;
     }
 };
 
 export const checkEmailExists = async (email) => {
     try {
-        const response = await axios.get(`http://localhost:8080/api/customers/check-email?email=${email}`);
+        const response = await axios.get(`http://localhost:8080/api/customers/check-email`, {
+            params: { email: email },
+        });
         if (response.data) {
             toast.error('Email đã tồn tại. Vui lòng sử dụng email khác.', {
                 position: "top-right",
